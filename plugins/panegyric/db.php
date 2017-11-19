8cda40c8c2dc0daa78ab2ab24e17fcca7cd2e94a
Tom Parker <palfrey@tevp.net>
2017-11-19 23:23:50 +0000

    Add required DB tables

---
+++ plugins/panegyric/db.php
@@ -0,0 +1,66 @@
+<?php
+global $panegyric_db_version;
+$panegyric_db_version = '1.0';
+
+function table_exists($name) {
+    global $wpdb;
+    $rows = $wpdb->query( "SHOW TABLES LIKE '$name';" );
+    if ($rows == 1)
+        return TRUE;
+    else
+        return FALSE;
+}
+
+function create_table($name, $sql) {
+    if (!table_exists($name)) {
+        global $wpdb;
+        $charset_collate = $wpdb->get_charset_collate();
+        $wpdb->query("CREATE TABLE $name ($sql) $charset_collate;");
+    }
+}
+
+function panegyric_table_install() {
+    global $wpdb;
+    global $panegyric_db_version;
+
+    $prefix = $wpdb->prefix . 'panegyric';
+    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+    create_table($prefix . "_tag_names", "
+        name VARCHAR(255),
+        org_list TEXT NOT NULL,
+        user_list TEXT NOT NULL,
+        exclude_user_list TEXT NOT NULL,
+        PRIMARY KEY  (name)");
+    create_table($prefix . "_org", "
+        org VARCHAR(39),
+        status ENUM('success', 'not-found', 'denied', 'not-checked'),
+        updated TIMESTAMP NULL,
+        PRIMARY KEY  (org)");
+    create_table($prefix . "_users", "
+        username VARCHAR(39),
+        org VARCHAR(39) NULL,
+        status ENUM('success', 'not-found', 'denied', 'not-checked'),
+        updated TIMESTAMP NULL,
+        prs_updated TIMESTAMP NULL,
+        PRIMARY KEY  (username),
+        FOREIGN KEY  (org) REFERENCES {$prefix}_org(org) ON DELETE CASCADE");
+    create_table($prefix . "_repo", "
+        id INT NOT NULL AUTO_INCREMENT,
+        name TEXT,
+        description TEXT,
+        url TEXT,
+        updated TIMESTAMP NULL,
+        PRIMARY KEY  (id)");
+    create_table($prefix . "_prs", "
+        id INT NOT NULL AUTO_INCREMENT,
+        url TEXT,
+        user VARCHAR(39) NOT NULL,
+        repo INT NOT NULL,
+        title TEXT NOT NULL,
+        PRIMARY KEY  (id),
+        FOREIGN KEY  (user) REFERENCES {$prefix}_users(username) ON DELETE CASCADE,
+        FOREIGN KEY  (repo) REFERENCES {$prefix}_repo(id) ON DELETE CASCADE");
+
+    add_option( 'panegyric_db_version', $panegyric_db_version );
+}
--- plugins/panegyric/panegyric.php
+++ plugins/panegyric/panegyric.php
@@ -16,4 +16,8 @@ function github_prs_func( $atts ) {
 	return "org = {$atts['org']}";
 }
 add_shortcode( 'github_prs', 'github_prs_func' );
+
+define( 'PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+include( PLUGIN_PATH . 'db.php');
+register_activation_hook( __FILE__, 'panegyric_table_install' );
 ?>
