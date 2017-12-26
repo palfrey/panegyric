fd83c48292e7194cae43874db645d253dc0ea66e
Tom Parker <palfrey@tevp.net>
2017-12-26 17:48:14 +0000

    Add custom format strings (fixes #4)

--- plugins/panegyric/panegyric.php
+++ plugins/panegyric/panegyric.php
@@ -21,6 +21,27 @@ function comma_split($instr)
     return array_filter(explode(",", $instr), 'strlen');
 }
 
+// based off https://bugs.php.net/bug.php?id=43901
+function expand_vars($str, $locals)
+{
+    while (true) {
+        $res=preg_replace_callback(
+            '#\{\$([A-Za-z_][A-Za-z0-9_]*)\}#',
+            function ($matches) use ($locals) {
+                if (!isset($locals[$matches[1]])) {
+                    die("Variable ".@$matches[1]." is undefined.");
+                }
+                return $locals[$matches[1]];
+            },
+            $str
+        );
+        if ($res == $str) {
+            return $str;
+        }
+        $str = $res;
+    }
+}
+
 function panegyric_shortcodes_init()
 {
     function github_prs_func($atts)
@@ -47,14 +68,29 @@ function panegyric_shortcodes_init()
         } else {
             $limit = 10;
         }
+        if (array_key_exists("format", $atts)) {
+            $format = $atts["format"];
+        } else {
+            $format = '{$updated_at}:
+                "<a href="{$pr_url}">{$pr_title}</a>" was done by
+                <a href="{$user_url}">{$name}</a> for <a href="{$repo_url}">{$repo_name}</a>';
+        }
+
         $prs = $db->get_prs($orgs, $users, $limit);
         $outstr = '<ul class="pangegyric-list">';
         foreach ($prs as $pr) {
-            $updated_at = DateTime::createFromFormat('Y-m-d H:i:s', $pr->updated_at);
-            $outstr .= "<li class=\"panegyric-item\">
-                {$updated_at->format('Y-m-d')}: \"<a href=\"{$pr->pr_url}\">{$pr->title}</a>\"
-                was done by <a href=\"https://github.com/{$pr->username}\">{$pr->name}</a>
-                for <a href=\"{$pr->repo_url}\">{$pr->repo_name}</a></li>";
+            $outstr .= expand_vars(
+                "<li class=\"panegyric-item\">".$format."</li>",
+                array(
+                    "name" => $pr->name,
+                    "pr_title" => $pr->title,
+                    "pr_url" => $pr->pr_url,
+                    "repo_name" => $pr->repo_name,
+                    "repo_url" => $pr->repo_url,
+                    "updated_at" => DateTime::createFromFormat('Y-m-d H:i:s', $pr->updated_at)->format('Y-m-d'),
+                    "user_url" => "https://github.com/{$pr->username}"
+                )
+            );
         }
         return $outstr . "</ul>";
     }
